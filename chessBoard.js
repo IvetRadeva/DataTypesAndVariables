dc3fde4f356d288288ae6f4cf29501666cf144cd
IvetRadeva <ivet.toleva@icloud.com>
2026-05-29 01:29:23 +0300

    Add files via upload

---
+++ chessBoard.js
@@ -0,0 +1,21 @@
+function solve(n) {
+    let result = '<div class="chessboard">\n';
+
+    for (let row = 0; row < n; row++) {
+        result += '<div>\n';
+
+        for (let col = 0; col < n; col++) {
+            if ((row + col) % 2 === 0) {
+                result += '    <span class="black"></span>\n';
+            } else {
+                result += '    <span class="white"></span>\n';
+            }
+        }
+
+        result += '</div>\n';
+    }
+
+    result += '</div>';
+    return result;
+}
+console.log(solve(3));
